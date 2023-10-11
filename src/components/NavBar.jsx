0f8f1453030e0bb517601ee196d252f28ee4f2bd
Idowu Emmanuel <idowuemmao@gmail.com>
2023-10-11 16:07:30 +0100

    Navbar responsiveness

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,29 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../images/logo.png";
 import { FaGithub, FaTwitter, FaInstagram, FaLinkedinIn } from "react-icons/fa";
 import { LiaFacebookF } from "react-icons/lia";
+import { AiOutlineMenu } from "react-icons/ai";
 
 const NavBar = () => {
-  // const [activeLink, setActiveLink] = useState("home");
+  const [isOpen, setIsOpen] = useState(false);
 
-  //to detect if the user has scrolled and change the color of the background
-  // const [scroll, setScroll] = useState(false);
-  //the useEffect will be triggered if the scroll starts
-  // useEffect(() => {
-  //   const onScroll = () => {
-  //     if (window.scrollY > 50) {
-  //       setScroll(true);
-  //     } else {
-  //       setScroll(false);
-  //     }
-  //   };
-  //   window.addEventListener("scroll", onScroll);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
-  //   return () => window.removeEventListener("scroll", onScroll);
-  // }, []);
   return (
-    // <header className={scroll ? "scroll" : "navbar"}>
-    <header className="navbar">
+    <nav className="navbar">
       <img src={logo} alt="logo" className="App__logo" />
       <div className="navbar__right">
         <ul className="nav__links">
@@ -46,7 +35,43 @@ const NavBar = () => {
         </div>
         <button className="connect">Let's Connect</button>
       </div>
-    </header>
+      <button onClick={toggleMenu} className="nav-btn">
+        <AiOutlineMenu />
+      </button>
+      {isOpen && (
+        <div className="md:hidden ">
+            <div className="navbar__right2">
+              <ul className="nav__links">
+                <li>
+                  <a href="/">Home</a>
+                </li>
+                <li>
+                  <a href="/">Skills</a>
+                </li>
+                <li>
+                  <a href="/">Projects</a>
+                </li>
+              </ul>
+              <div className="nav-icons">
+                <FaLinkedinIn className="icons" />
+                <LiaFacebookF className="icons" />
+                <FaInstagram className="icons" />
+                <FaTwitter className="icons" />
+                <FaGithub className="icons" />
+              </div>
+              <button className="connect">Let's Connect</button>
+            </div>
+            <div>
+              <button
+                onClick={toggleMenu}
+                className="text-accent hover:text-accent focus:text-accent focus:outline-none"
+              >
+                <AiOutlineMenu />
+              </button>
+            </div>
+          </div>
+      )}
+    </nav>
   );
 };
 
