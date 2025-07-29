e0c95eed734cfaa605bb8b83af18e70754025265
Idowu Emmanuel <idowuemmao@gmail.com>
2025-07-29 17:09:26 +0100

    updated the CV link

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -57,7 +57,7 @@ const Banner = () => {
           data-aos="zoom-in"
           data-aos-easing="ease-out-cubic"
           data-aos-duration="1500"
-          href="https://drive.google.com/file/d/1yojkx0181mO3VCEvZ-MaQKHib_YW6lsJ/view?usp=sharing"
+          href="https://drive.google.com/file/d/1W4az5TUpIEIppYx5BS8g_6egqurc7lc9/view?usp=sharing"
           target="_blank"
           rel="noopener noreferrer"
           className="flex items-center gap-4 p-2 cursor-pointer px-8 rounded-xl bg-primary w-fit hover:underline"
